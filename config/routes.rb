9f7af2cc5e8a88ec4439c822301bbebc6a937d06
Merit Victor <meritsvictor1.618@gmail.com>
2017-04-09 16:02:39 +0000

    add home, about, user using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  root 'static_pages#home'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
