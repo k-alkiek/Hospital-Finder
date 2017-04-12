73ffb6f99d341384a4a7347a44d3e8690a54966c
Merit Victor <meritsvictor1.618@gmail.com>
2017-04-12 12:03:57 +0000

    Add geocoding to hospitals

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,6 @@
 class Hospital < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
