4b768e611a276ed415868413f15acee184bda779
Merit Victor <meritsvictor1.618@gmail.com>
2017-04-15 18:53:17 +0000

    Add map

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @hospitals = Hospital.all
+    @hash = Gmaps4rails.build_markers(@hospitals) do |hospital, marker|
+    marker.lat hospital.latitude
+    marker.lng hospital.longitude
+
+end
   end
 
   def about
