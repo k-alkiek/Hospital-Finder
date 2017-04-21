4b2f1f9aa9d19ad1d4b78f03f4f333047e0e8820
Merit Victor <meritsvictor1.618@gmail.com>
2017-04-21 19:15:57 +0000

    H Add hospital page design

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,6 +1,11 @@
 class HospitalsController < ApplicationController
   def show
     @hospital = Hospital.find(params[:id])
+    @hospitals = [@hospital]
+    @hash = Gmaps4rails.build_markers(@hospitals) do |hospital, marker|
+      marker.lat hospital.latitude
+      marker.lng hospital.longitude
+    end
   end
   
   # helper_method :resource_name, :resource, :devise_mapping
